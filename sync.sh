8995242681990172a9a6a991efc98cd407471d09
SkyForge_ <goldr1000ftw@gmail.com>
2017-08-25 18:54:05 -0500

    Added bot sync functionality and scripts

--- sync.sh
+++ sync.sh
@@ -1,4 +1,20 @@
 cd
+
 rm -rfv /var/www/html/*
 cp -r /home/_theforge/_git/TheForge/website/* /var/www/html
 echo "Deleted old, and Transfered website files."
+
+rm /home/_theforge/_discord/_bots/TheForge/bot.js
+cp -r /home/_theforge/_git/TheForge/discord/bot.js /home/_theforge/_discord/_bots/TheForge
+echo "Updated bot."
+
+rm -rfv /home/_theforge/_servers/minecraft/lobby/plugins/Skript/scripts/*
+rm -rfv /home/_theforge/_servers/minecraft/RPG/plugins/Skript/scripts/*
+rm -rfv /home/_theforge/_servers/minecraft/survival/plugins/Skript/scripts/*
+cp -r /home/_theforge/_git/minecraft/scripts/global/* /home/_theforge/_servers/minecraft/lobby/plugins/Skript/scripts
+cp -r /home/_theforge/_git/minecraft/scripts/global/* /home/_theforge/_servers/minecraft/RPG/plugins/Skript/scripts
+cp -r /home/_theforge/_git/minecraft/scripts/global/* /home/_theforge/_servers/minecraft/survival/plugins/Skript/scripts
+cp -r /home/_theforge/_git/minecraft/scripts/lobby/* /home/_theforge/_servers/minecraft/lobby/plugins/Skript/scripts
+cp -r /home/_theforge/_git/minecraft/scripts/RPG/* /home/_theforge/_servers/minecraft/RPG/plugins/Skript/scripts
+cp -r /home/_theforge/_git/minecraft/scripts/survival/* /home/_theforge/_servers/minecraft/survival/plugins/Skript/scripts
+echo "Replaced all scripts on all servers."
