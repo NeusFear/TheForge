df51c5b58d880b1ffd9ed200381ecd42964aa206
SkyForge_ <goldr1000ftw@gmail.com>
2017-08-25 18:36:30 -0500

    that wasnt oops

--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
 cd
 rm -rfv /var/www/html/*
-cp -r /home/_theforge/_git/TheForge/website /var/www/html
+cp -r /home/_theforge/_git/TheForge/website/* /var/www/html
 echo "Deleted old, and Transfered website files."
