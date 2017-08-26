9465a33a35d076cfbd4983281c0c02258a86ca67
SkyForge_ <goldr1000ftw@gmail.com>
2017-08-25 21:51:07 -0500

    Re: Fix

--- website/index.php
+++ website/index.php
@@ -88,10 +88,10 @@
 
           <div class="parallax" id="parallax-5"></div>
 
-          <div class="connect" style="height: 1200px;">
+          <div class="connect">
             <br>
             <div class="splash-left"><font color="#ffc342">Connect with us:</font><br /></div>
-            <div class="info" style="margin-left: 20%;"><img src="images/Icons/social media/051-discord.png" /><br />Discord<p>Description of many words will go here. blah blah blah</p></div>
+            <div class="info"><img src="images/Icons/social media/051-discord.png" /><br />Discord<p>Description of many words will go here. blah blah blah</p></div>
             <div class="info"><img src="images/Icons/social media/051-twitter.png" /><br />Twitter<p>Description of many words will go here.</p></div>
             <div class="info"><img src="images/Icons/social media/051-youtube.png" /><br />YouTube<p>Description of many words will go here.</p></div>
           </div>
