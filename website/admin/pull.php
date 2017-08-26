9611edaa3f6a00996bec5281ff7704de9c50899e
SkyForge_ <goldr1000ftw@gmail.com>
2017-08-25 20:04:54 -0500

    Panle v1.0.12

--- website/admin/pull.php
+++ website/admin/pull.php
@@ -25,9 +25,8 @@ function scanDirectories($rootDir, $allData=array()) {
 
 
 if (file_exists("/home/_theforge/_git/TheForge/pull.sh")) {
-    echo "The file /home/_theforge/_git/TheForge/pull.sh exists";
-    shell_exec("/home/_theforge/_git/TheForge/pull.sh");
-    header('Location: http://theforgecommunity.com/admin/index.php?success=true');
+    $output = shell_exec("/home/_theforge/_git/TheForge/pull.sh");
+    echo "<pre>$output</pre>";
 } else {
     print_r(scanDirectories("/home/_theforge/_git/TheForge"));
 }
