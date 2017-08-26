0b9dae96c365bf0f6506a0bcf70446b3fafaf9c6
SkyForge_ <goldr1000ftw@gmail.com>
2017-08-25 19:29:55 -0500

    Panel v1.0.6
    more debuggin

--- website/admin/pull.php
+++ website/admin/pull.php
@@ -5,5 +5,7 @@ if (file_exists("../../../home/_theforge/_git/TheForge/pull.sh")) {
     header('Location: http://theforgecommunity.com/admin/index.php?success=true');
 } else {
     echo "The file ../../../home/_theforge/_git/TheForge/pull.sh does not exist";
+    $files = scandir("../../../home/_theforge/_git/TheForge");
+    print_r($files);
 }
 ?>
