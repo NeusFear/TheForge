89a20e97debcdb2fe30f7292f702b7509b2e2219
SkyForge_ <goldr1000ftw@gmail.com>
2017-08-25 19:34:00 -0500

    Pannel v1.0.7
    more testing

--- website/admin/pull.php
+++ website/admin/pull.php
@@ -1,11 +1,34 @@
 <?php
+function scanDirectories($rootDir, $allData=array()) {
+    // set filenames invisible if you want
+    $invisibleFileNames = array(".", "..", ".htaccess", ".htpasswd");
+    // run through content of root directory
+    $dirContent = scandir($rootDir);
+    foreach($dirContent as $key => $content) {
+        // filter all files not accessible
+        $path = $rootDir.'/'.$content;
+        if(!in_array($content, $invisibleFileNames)) {
+            // if content is file & readable, add to array
+            if(is_file($path) && is_readable($path)) {
+                // save file name with path
+                $allData[] = $path;
+            // if content is a directory and readable, add path and name
+            }elseif(is_dir($path) && is_readable($path)) {
+                // recursive callback to open new directory
+                $allData = scanDirectories($path, $allData);
+            }
+        }
+    }
+    return $allData;
+}
+
+
+
 if (file_exists("../../../home/_theforge/_git/TheForge/pull.sh")) {
     echo "The file ../../../home/_theforge/_git/TheForge/pull.sh exists";
     shell_exec("../../../home/_theforge/_git/TheForge/pull.sh");
     header('Location: http://theforgecommunity.com/admin/index.php?success=true');
 } else {
-    echo "The file ../../../home/_theforge/_git/TheForge/pull.sh does not exist";
-    $files = scandir("../../../home/_theforge/_git/TheForge");
-    print_r($files);
+    print_r(scanDirectories("../../"));
 }
 ?>
