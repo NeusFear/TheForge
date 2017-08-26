1a2822b9bf8a39a82f2307fbeedb4fce3eea2237
SkyForge_ <goldr1000ftw@gmail.com>
2017-08-25 19:19:53 -0500

    panel v1

---
+++ website/admin/index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+  <head>
+
+      <!--Fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet"></link>
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet"></link>
+
+      <!--Styles-->
+    <link href="css/style.css" rel="stylesheet"></link>
+
+  </head>
+  <body>
+    <form action="push.php">
+      <input type="submit" value="Push">
+    </form>
+    <form action="sync.php">
+      <input type="submit" value="Push">
+    </form>
+  </body>
+<html>
---
+++ website/admin/pull.php
@@ -0,0 +1,4 @@
+<?php
+shell_exec("../../../../home/_theforge/_git/TheForge/pull.sh");
+header('Location: http://theforgecommunity.com/admin/index.php?success=true');
+?>
