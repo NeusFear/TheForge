27b69f865a1d69303f54e020f5bd525b47a431e7
SkyForge_ <goldr1000ftw@gmail.com>
2017-08-25 20:14:13 -0500

    test on sync

--- website/admin/sync.php
+++ website/admin/sync.php
@@ -1,4 +1,33 @@
 <?php
-shell_exec("../../../home/_theforge/_git/TheForge/sync.sh");
-header('Location: http://theforgecommunity.com/admin/index.php?success=true');
+function scanDirectories($rootDir, $allData=array()) {
+    // set filenames invisible if you want
+    $invisibleFileNames = array(".", "..", ".htaccess", ".htpasswd");
+    // run through content of root directory
+    $dirContent = scandir($rootDir);
+    foreach($dirContent as $key => $content) {
+        // filter all files not accessible
+        $path = $rootDir.'/'.$content;
+        if(!in_array($content, $invisibleFileNames)) {
+            // if content is file & readable, add to array
+            if(is_file($path) && is_readable($path)) {
+                // save file name with path
+                $allData[] = $path;
+            // if content is a directory and readable, add path and name
+            }elseif(is_dir($path) && is_readable($path)) {
+                // recursive callback to open new directory
+                $allData = scanDirectories($path, $allData);
+            }
+        }
+    }
+    return $allData;
+}
+
+
+
+if (file_exists("/home/_theforge/_git/TheForge/sync.sh")) {
+    $output = shell_exec("/home/_theforge/_git/TheForge/sync.sh");
+    echo "<pre>$output</pre>";
+} else {
+    print_r(scanDirectories("/home/_theforge/_git/TheForge"));
+}
 ?>
