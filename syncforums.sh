24cf617a3f45bb1e6264bd9dda48bc40909a06b5
SkyForge_ <goldr1000ftw@gmail.com>
2017-09-07 17:47:53 -0500

    Dont use github the same for forums

--- syncforums.sh
+++
@@ -1,7 +0,0 @@
-cd
-
-rm -rfv /var/www/html/forums/*
-cp -r /home/_theforge/_git/TheForge/website/forums/* /var/www/html/forums
-echo "Deleted old, and Transfered forums files."
-
-echo "Sync complete."
