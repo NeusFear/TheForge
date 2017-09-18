e25a99743cf109bd6730c3d8f448b40139778b24
SkyForge_ <goldr1000ftw@gmail.com>
2017-09-18 09:21:30 -0500

    new path

--- sync.sh
+++ sync.sh
@@ -2,22 +2,22 @@ cd
 
 shopt -s extglob
 rm -rf !(forums)
-cp -r /home/_theforge/_git/TheForge/website/* /var/www/html
+cp -r /home/_theforge/_git/website/* /var/www/html
 echo "Deleted old, and Transfered website files."
 
-rm /home/_theforge/_discord/_bots/TheForge/bot.js
-cp -r /home/_theforge/_git/TheForge/discord/bot.js /home/_theforge/_discord/_bots/TheForge
+rm /home/_theforge/_discord/_bots/bot.js
+cp -r /home/_theforge/_git/discord/bot.js /home/_theforge/_discord/_bots
 echo "Updated bot."
 
 rm -rfv /home/_theforge/_servers/minecraft/lobby/plugins/Skript/scripts/*
 rm -rfv /home/_theforge/_servers/minecraft/RPG/plugins/Skript/scripts/*
 rm -rfv /home/_theforge/_servers/minecraft/survival/plugins/Skript/scripts/*
-cp -r /home/_theforge/_git/TheForge/minecraft/scripts/global/* /home/_theforge/_servers/minecraft/lobby/plugins/Skript/scripts
-cp -r /home/_theforge/_git/TheForge/minecraft/scripts/global/* /home/_theforge/_servers/minecraft/RPG/plugins/Skript/scripts
-cp -r /home/_theforge/_git/TheForge/minecraft/scripts/global/* /home/_theforge/_servers/minecraft/survival/plugins/Skript/scripts
-cp -r /home/_theforge/_git/TheForge/minecraft/scripts/lobby/* /home/_theforge/_servers/minecraft/lobby/plugins/Skript/scripts
-cp -r /home/_theforge/_git/TheForge/minecraft/scripts/RPG/* /home/_theforge/_servers/minecraft/RPG/plugins/Skript/scripts
-cp -r /home/_theforge/_git/TheForge/minecraft/scripts/survival/* /home/_theforge/_servers/minecraft/survival/plugins/Skript/scripts
+cp -r /home/_theforge/_git/minecraft/scripts/global/* /home/_theforge/_servers/minecraft/lobby/plugins/Skript/scripts
+cp -r /home/_theforge/_git/minecraft/scripts/global/* /home/_theforge/_servers/minecraft/RPG/plugins/Skript/scripts
+cp -r /home/_theforge/_git/minecraft/scripts/global/* /home/_theforge/_servers/minecraft/survival/plugins/Skript/scripts
+cp -r /home/_theforge/_git/minecraft/scripts/lobby/* /home/_theforge/_servers/minecraft/lobby/plugins/Skript/scripts
+cp -r /home/_theforge/_git/minecraft/scripts/RPG/* /home/_theforge/_servers/minecraft/RPG/plugins/Skript/scripts
+cp -r /home/_theforge/_git/minecraft/scripts/survival/* /home/_theforge/_servers/minecraft/survival/plugins/Skript/scripts
 echo "Replaced all scripts on all servers."
 
 echo "Sync complete."
