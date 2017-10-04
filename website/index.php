d5cdec30261d13836ca67fae3f21b256a11376e1
SkyForge_ <goldr1000ftw@gmail.com>
2017-10-03 21:52:23 -0500

    Website 2.0.0-dev1 | Initial Launch

--- website/index.php
+++ website/index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8"></meta>
       <!--Tab Title-->
-    <title>The Forge</title>
+    <title>TheForge - Home</title>
 
       <!--Meta-->
     <meta content="The Forge - Online Community" name="description"></meta>
@@ -20,104 +20,16 @@
     <link href="css/style.css" rel="stylesheet"></link>
 
       <!--Scripts-->
-    <script src="bot.js"></script>
     <script src="js/main.js"></script>
   </head>
 
-      <!--Page 1-->
-  <body>
-    <div class="section" id="1" style="display: block;">
-      <div id="header" class="header"><font color="#ffc342">THEFORGE</font>HOME</div>
-
-      <div class="parallax" id="parallax-1" style="min-height:680px;">
-        <div class="splash"><div style="font-size:60px;"><font color="#ffc342">welcome</font><small>to</small></div><div style="font-size:122px;"><small>THE</small><font color="#ffc342">FORGE</font></div></div>
-      </div>
-
-      <div class="transition">
-        <div class="splash-left"><font color="#ffc342">Who are we?</font><br /></div>
-        <p>We are a new modern gaming community that aims to make a fun and helpful experience for today's video game loving world.</p>
-        <span class="flex" style="display:block;">
-          <div class="info"><img src="images/Icons/circular/success.png" /><br />Modern<p>We aim to keep at the forefront of developments in gaming.</p></div>
-          <div class="mobilebreaks"><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br></div>
-          <div class="info"><img src="images/Icons/circular/list.png" /><br />Forums<p>To keep progressive gamers connected in a like-minded community.</p></div>
-          <div class="mobilebreaks"><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br></div>
-          <div class="info"><img src="images/Icons/circular/internet.png" /><br />Servers<p>Our community hosts dedicated servers avalible to anyone.</p></div>
-        </span>
-        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-        <span class="flex" style="display:block;">
-          <div class="info"><img src="images/Icons/circular/volume.png" /><br />Discord<p>The fastest way to communicate with other members, and recieve updates on server information.</p></div>
-          <div class="mobilebreaks"><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br></div>
-          <div class="info"><img src="images/Icons/circular/email.png" /><br />Staff<p>Our helpful staff endeavors to resolve all issues and answer all questions as quickly and helpfully as possible.</p></div>
-          <div class="mobilebreaks"><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br></div>
-          <div class="info"><img src="images/Icons/circular/earth-globe.png" /><br />Community<p>The main goal of our community is to enjoy the games we play as well as provide helpful feedback on those games.</p></div>
-        </span>
-      </div>
-
-      <div class="parallax" id="parallax-2"></div>
-      <div class="parallax" id="parallax-3"></div>
-
-      <div class="connect">
-        <br>
-        <div class="splash-left"><font color="#ffc342">Connect with us:</font><br /></div>
-        <span class="flex" style="display:block;">
-          <div class="info"><img src="images/Icons/social media/051-discord.png" /><br />Discord<p>Description of many words will go here. blah blah blah</p></div>
-          <div class="info"><img src="images/Icons/social media/051-twitter.png" /><br />Twitter<p>Description of many words will go here.</p></div>
-          <div class="info"><img src="images/Icons/social media/051-youtube.png" /><br />YouTube<p>Description of many words will go here.</p></div>
-        </span>
-      </div>
-
-    </div>
-
-
-
-
-
-    <div class="section" id="2" style="display:none;">
-      <div id="header" class="header"><font color="#ffc342">THEFORGE</font>COMMUNITY</div>
-        <div id="content" class="content">
-
-          <div class="parallax" id="parallax-4" style="min-height:50px;"></div>
-
-          <div class="transition" id="communitytransition">
-            <div class="splash-left"><font color="#ffc342">COMMUNITY</font><br /></div>
-            <p>Our community hosts dedicated servers avalible to anyone</p>
-            <span class="flex" style="display:block;">
-              <div class="info"><div class="icon"><img src="images/minecrafticon.png" /></div><br />Minecraft<p>theforgemc.com (Currently under development)</p></div>
-              <div class="info"><div class="icon"><img src="images/arkicon.png" /></div><br />Ark<p>theforgeark.com</p><br /><form action="arktutorial.php"><input class="submit" type="submit" value="Ark Help"></form></div>
-              <div class="info"><div class="icon"><img src="images/Icons/construction/engineer.png" /></div><br />Coming Soon...<p>more servers to come..</p></div>
-            </span>
-          </div>
-
-          <div class="parallax" id="parallax-5"></div>
-
-          <div class="connect">
-            <br>
-            <div class="splash-left"><font color="#ffc342">Connect with us:</font><br /></div>
-            <span class="flex" style="display:block;">
-              <div class="info"><img src="images/Icons/social media/051-discord.png" /><br />Discord<p>Description of many words will go here. blah blah blah</p></div>
-              <div class="info"><img src="images/Icons/social media/051-twitter.png" /><br />Twitter<p>Description of many words will go here.</p></div>
-              <div class="info"><img src="images/Icons/social media/051-youtube.png" /><br />YouTube<p>Description of many words will go here.</p></div>
-            </span>
-          </div>
-
-        </div>
-    </div>
-
-
-
-
-
-
-
-    <div class="nav" id="nav">
-      <input class="navbutton" id="left" type="button" value="&#9664;" onclick="left();" style="float:left;background:transparent;" />
-      <div class="pages" id="things" style="float:left;">
-        Stuff n' things
-      </div>
-      <input class="navbutton" id="right" type="button" value="&#9654;" onclick="right();" style="float:left;background:transparent;" />
-    </div>
-
-
-
+  <body id="root">
+    <?php include 'modules/header.html'?>
+    <br>
+    <?php include 'modules/navbar.html'?>
+    <br><br>
+    <?php include 'modules/aboutus.html'?>
+    <?php include 'modules/whyus.html'?>
+    <?php include 'modules/followus.html'?>
+    <!--<?php include 'modules/footer.html'?>-->
   </body>
-</html>
