c6352eaeefc43f6a4ba7cc477fcfcb4a14b25cdb
Brandon <Goldr1000ftw@gmail.com>
2017-10-05 11:05:49 -0500

    Website v2.2.0 -dev6

---
+++ website/modules/forumsofftopic.php
@@ -0,0 +1,7 @@
+<div class="content">
+  <div class="contentheader"><img src="images/Icons/general/085-info.png" style="width:28px;height:28px;"> OFF TOPIC</div>
+  <p>
+    Categories-
+    <?php include 'modules/category.html'?>
+  </p>
+</div>
