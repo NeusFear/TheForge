653416512d7294ca81ed41ec1592784faa297a91
SkyForge_ <goldr1000ftw@gmail.com>
2017-10-05 18:15:43 -0500

    Website | v2.2.0 - dev8

--- website/modules/category.php
+++ website/modules/category.php
@@ -1,14 +1,12 @@
-<div class="content">
-	<div class="category">
-		<img src="images/Icons/general/085-info.png" style="margin-left:-90px;width:60px;height:60px;border-radius:30px;float:left;">
-		<span style="display:inline-block;">
-			<div class="categorytitle"><h1 style="margin-bottom: -8px;">Category Title</h1></div>
-			<p style="margin-top:0px;">
-				Description of topic
-			</p>
-		</span>
-		<span style="float:right;display:inline-block;color:#a0a0a0;"><br>
-			0
-		</span>
-	</div><br>
-</div>
+<div class="category">
+	<img src="images/Icons/general/066-send.png" style="margin-left:-70px;width:60px;height:60px;border-radius:30px;float:left;">
+	<span style="display:inline-block;">
+		<div class="categorytitle"><h2 style="margin-bottom: -8px;">Category Title</h2></div>
+		<p style="margin-top:0px;">
+			Description of topic
+		</p>
+	</span>
+	<span style="float:right;display:inline-block;color:#a0a0a0;"><br>
+		0 Topics
+	</span>
+</div><br>
--- website/modules/forumsofftopic.php
+++ website/modules/forumsofftopic.php
@@ -1,7 +1,6 @@
 <div class="content">
-  <div class="contentheader"><img src="images/Icons/general/085-info.png" style="width:28px;height:28px;"> OFF TOPIC</div>
+  <div class="contentheader"><img src="images/Icons/general/171-folder-3.png" style="width:28px;height:28px;"> OFF TOPIC</div>
   <p>
-    Categories-
-    <?php include 'modules/category.html'?>
+    <?php include 'category.php'?>
   </p>
 </div>
