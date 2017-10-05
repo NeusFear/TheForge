e184caec4bff0cb2fbbe29df73857828ecc89e5e
Brandon <Goldr1000ftw@gmail.com>
2017-10-05 10:47:22 -0500

    Website | v2.2.0 - dev1
    Adding forums index

---
+++ website/forums.php
@@ -0,0 +1,36 @@
+
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8"></meta>
+      <!--Tab Title-->
+    <title>TheForge - Forums</title>
+
+      <!--Meta-->
+    <meta content="The Forge - Online Community" name="description"></meta>
+    <meta content="SkyForge_" name="author"></meta>
+
+      <!--Favicon-->
+    <link href="images/icons/favicon.ico" rel="shortcut icon"></link>
+
+      <!--Fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet"></link>
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet"></link>
+
+      <!--Styles-->
+    <link href="css/style.css" rel="stylesheet"></link>
+
+      <!--Scripts-->
+    <script src="js/main.js"></script>
+  </head>
+
+  <body id="root">
+    <?php include 'modules/header.html'?>
+    <br>
+    <?php include 'modules/navbar.html'?>
+    <br><br>
+    <?php include 'modules/forumsinfo.html'?>
+    <?php include 'modules/forumsgeneral.html'?>
+    <?php include 'modules/forumsgaming.html'?>
+    <?php include 'modules/forumsofftopic.html'?>
+   </body>
