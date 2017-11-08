3504b946ebcb3cc0f098c5ca4b883d6a3bb1625d
Brandon <Goldr1000ftw@gmail.com>
2017-11-07 20:55:03 -0600

    Bot | Added and removed more info I noticed

--- discord/bot.js
+++ discord/bot.js
@@ -150,9 +150,9 @@ bot.on('message', message => {
         .setThumbnail(bot.user.avatarURL)
         .addField("Rules:", "**1)** Use the channels for their specified topic, please...\n**2)** Don't spam our text channels, no one wants all those notifications...\n**3)** If you have WWIII going on in the background, don't join a Voice Channel.\n**4)** Keep the racial slurs to a minimum.")
         .addField("Channels:", "**#welcome** : This channel is for infomation... you're reading it now.\n**#announcements** : Find out what's new in the community.\n**#ideas** : Submit suggestions here\n**#random** : Used for, casual chat, dank memes, links, etc.")
-        .addField("Other Channels:", "**#ark** : For our (Currently in Development) Server\n**#minecraft** : For our custom *@Minecraft* (Currently in Development)")
+        .addField("Other Channels:", "**#ark** : For our public Server\n**#minecraft** : For our custom *@Minecraft* (Currently in Development)")
         .addField("Roles:", "**@Admin** - who manage the game serevrs, website, and forums\n**@Moderator** - keep those pesky trolls under their bridges\n**@Associate** - Sometimes we associate with them.\n**@<Game>** - People who play a game. (*duh*)")
-        .addField("Website:", "*Our website is currently being made:*\nProject led by: **@SkyForge**")
+        .addField("Website:", "*!website*")
         .addField("Our Dedicated Servers", "**Ark:** *theforgeark.com*\n**Minecraft:** *coming soon*")
 
         message.channel.send({embed});
