f484d1c063cfa85a45bef582fec28c4998c03b07
Brandon <Goldr1000ftw@gmail.com>
2017-11-10 12:15:51 -0600

    Website | 2.0.0 - dev21

---
+++ website/modules/profilehome.php
@@ -0,0 +1,22 @@
+<div class="content">
+  <div class="contentheader"><img src="images/Icons/general/306-levels.png" style="width:28px;height:28px;"> Username goes here</div>
+  <p>
+    <div class="columns">
+      <span>
+        <div style="background-color:white;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);margin-left:15%;margin-right:15%;margin-top:-6%;padding: 5%;">
+          <h1>Title</h1>Info<br><br>
+        </div>
+      </span>
+      <span>
+        <div style="background-color:white;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);margin-left:15%;margin-right:15%;margin-top:-6%;padding: 5%;">
+          <h1>Title</h1>Info<br><br>
+        </div>
+      </span>
+      <span>
+        <div style="background-color:white;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);margin-left:15%;margin-right:15%;margin-top:-6%;padding: 5%;">
+          <h1>Title</h1>Info<br><br>
+        </div>
+      </span>
+    </div>
+  </p>
+</div>
