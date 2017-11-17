42ef30efe07d50ca3f1e390ef0393c9d56258e35
SkyForge_ <goldr1000ftw@gmail.com>
2017-11-16 21:57:17 -0600

    Website | Additions to Profile home

--- website/modules/profilehome.php
+++ website/modules/profilehome.php
@@ -1,11 +1,11 @@
-<div class="content" style='background-image:url("images/banner.png"); background-repeat: no-repeat; background-attachment: fixed;background-position: center; background-size:100%;'>
+<div class="content" style='background-image:url("images/banner.png"); background-repeat: no-repeat; background-attachment: fixed; background-justify: center; background-size:100%;'>
   <p>
     <div class="columns">
 		<span>
 			<div class="rows">
 				<div class="card">
 					<img src="images/Icons/general/120-notebook.png" style="width:60px;height:60px;"><br>
-					<h1>BIO</h1>blhaaf alksdf lpwep lakahwek la ;a<br>sdkjf<br> oweo lald kfiwauiey kasdl lafl k blhaaf a<br>lksdf lpwep lakahwek la ;asdkjf oweo <br>aaf a<br>lksdf lpwep lakahwek la ;asdkjf oweo <br>aaf a<br>lksdf lpwep lakahwek la ;asdkjf oweo <br>aaf a<br>lksdf lpwep lakahwek la ;asdkjf oweo <br>aaf a<br>lksdf lpwep lakahwek la ;asdkjf oweo <br>aaf a<br>lksdf lpwep lakahwek la ;asdkjf oweo <br>aaf a<br>lksdf lpwep lakahwek la ;asdkjf oweo <br>aaf a<br>lksdf lpwep lakahwek la ;asdkjf oweo <br>aaf a<br>lksdf lpwep lakahwek la ;asdkjf oweo <br>aaf a<br>lksdf lpwep lakahwek la ;asdkjf oweo <br>aaf a<br>lksdf lpwep lakahwek la ;asdkjf oweo <br>aaf a<br>lksdf lpwep lakahwek la ;asdkjf oweo <br>aaf a<br>lksdf lpwep lakahwek la ;asdkjf oweo <br>aaf a<br>lksdf lpwep lakahwek la ;asdkjf oweo <br>lald kfiwauiey kasdl lafl k blhaaf alksdf lpwep lakahwek la ;asdkjf <br>oweo lald kfiwauiey kasdl lafl <br>kblhaaf alksdf lpwep lakahwek la ;asdkjf oweo lald kfiwauiey kasdl lafl k<br><br>
+					<h1>BIO</h1>He was bored. So bored. His great intellect, seemingly inexhaustible, was hungry for new challenges but he was the last of the great innovators - society's problems had all been solved. All seemingly unconnected disciplines had long since been found to be related in horrifically elusive and contrived ways and he had mastered them all. He lay back in the dark and tried to relax. He longed for the challenges of the past when his racing mind prevented him from sleeping. It was tortuous at the time, but he now looked back on those times enviously. Then it hit him. Since he couldn't be presented with a challenging problem any more, all he had to do was to create a problem of his own design; so complicated and with so many interrelated complex relationships that only he could untangle them and calculate the ultimate outcome. But where to start? Then it hit him. He opened his eyes in the dark and said, "Let there be light."<br><br>
 				</div>
 			</div>
 		</span>
@@ -13,7 +13,11 @@
 			<div class="rows">
 				<div class="card">
 				  <img src="images/Icons/general/120-notebook.png" style="width:60px;height:60px;"><br>
-				  <h1>STATS</h1>Followers Following Posts<br><br>
+				  <h1>STATS</h1>
+				  <div class="forgelist"><div style="float: left;">Followers</div><div style="float: right;">0</div></div><br>
+				  <div class="forgelist"><div style="float: left;">Following</div><div style="float: right;">0</div></div><br>
+				  <div class="forgelist"><div style="float: left;">Posts</div><div style="float: right;">0</div></div>
+				  <br><br>
 				</div>
 			</div>
 			<div class="rows">
