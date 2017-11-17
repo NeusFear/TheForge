69e9580892c5b606f7d5cf942e00f4fd8e919752
SkyForge_ <goldr1000ftw@gmail.com>
2017-11-16 19:45:57 -0600

    Website | Comit temp changes to respective locations

--- website/modules/login.php
+++ website/modules/login.php
@@ -6,25 +6,25 @@
         <img src="images/Icons/general/162-id-card.png" style="width:60px;height:60px;"><br>
         <div style="background-color:white;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);margin-left:15%;margin-right:15%;margin-top:-6%;padding: 5%;">
           <h1>Login</h1>Login to TheForge to stay connected with the rest of the community.<br><br>
-        </div>
         <form action="/profile.php">
           Username: <br><input type="text" name="Username"><br>
-          Password: <br><input type="text" name="Password"><br>
+          Password: <br><input type="password" name="Password"><br>
           <input type="submit" value="Login">
         </form>
+	  </div>
       </span>
       <span>
         <img src="images/Icons/general/162-id-card.png" style="width:60px;height:60px;"><br>
         <div style="background-color:white;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);margin-left:15%;margin-right:15%;margin-top:-6%;padding: 5%;">
           <h1>Create Account</h1>Create an account to be recognised by other users on TheForge.<br><br>
+		  <form action="/profile.php">
+			  Username: <br><input type="text" name="Username"><br>
+			  Email: <br><input type="text" name="Email"><br>
+			  Password: <br><input type="password" name="Password"><br>
+			  Confirm Password: <br><input type="password" name="Confirm"><br>
+			  <input type="submit" value="Create Account">
+		  </form>
         </div>
-        <form action="/profile.php">
-          Username: <br><input type="text" name="Username"><br>
-          Email: <br><input type="text" name="Email"><br>
-          Password: <br><input type="text" name="Password"><br>
-          Confirm Password: <br><input type="text" name="Confirm"><br>
-          <input type="submit" value="Create Account">
-        </form>
       </span>
     </div>
   </p>
--- website/modules/profilehome.php
+++ website/modules/profilehome.php
@@ -1,20 +1,25 @@
+<div class="content"><div class="contentheader"><img src="images/Icons/general/306-levels.png" style="width:28px;height:28px;"> <?php echo $_GET['user']?>'s Profile Page</div></div>
+
+<div class="content"><img alt="banner" src="BANNER" style="width:100%;height:10%;"></img></div>
+
 <div class="content">
-  <div class="contentheader"><img src="images/Icons/general/306-levels.png" style="width:28px;height:28px;"> Username goes here</div>
   <p>
     <div class="columns">
       <span>
+		<img src="images/Icons/general/120-notebook.png" style="width:60px;height:60px;"><br>
         <div style="background-color:white;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);margin-left:15%;margin-right:15%;margin-top:-6%;padding: 5%;">
-          <h1>Title</h1>Info<br><br>
+          <h1>BIO</h1>blhaaf alksdf lpwep lakahwek la ;asdkjf oweo lald kfiwauiey kasdl lafl k blhaaf alksdf lpwep lakahwek la ;asdkjf oweo lald kfiwauiey kasdl lafl k blhaaf alksdf lpwep lakahwek la ;asdkjf oweo lald kfiwauiey kasdl lafl kblhaaf alksdf lpwep lakahwek la ;asdkjf oweo lald kfiwauiey kasdl lafl k<br><br>
         </div>
       </span>
       <span>
+		<img src="images/Icons/general/206-chat.png" style="width:60px;height:60px;"><br>
         <div style="background-color:white;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);margin-left:15%;margin-right:15%;margin-top:-6%;padding: 5%;">
-          <h1>Title</h1>Info<br><br>
-        </div>
-      </span>
-      <span>
-        <div style="background-color:white;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);margin-left:15%;margin-right:15%;margin-top:-6%;padding: 5%;">
-          <h1>Title</h1>Info<br><br>
+          <h1>ACCOUNTS</h1>
+		  <img src="images/Icons/favicon.ico" style="width:20px;height:20px;border-radius:10px;"><br>TheForge: username
+		  <br><img src="images/Icons/social media/051-discord.png" style="width:20px;height:20px;"><br>Discord: blah#1234
+		  <br><img src="images/Icons/social media/051-twitch.png" style="width:20px;height:20px;"><br>Twitch: @yernan
+		  <br><img src="images/Icons/social media/051-youtube.png" style="width:20px;height:20px;"><br>Youtube: channel
+		  <br><br>
         </div>
       </span>
     </div>
